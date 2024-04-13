6962c7c9241cd7701c283db86ad3c102f0a5d7f2
Pratik Nirgun <158174740+HectiK07@users.noreply.github.com>
2024-04-13 16:28:34 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+// const leftbtn = document.querySelector("#left_btn");
+// const rightbtn = document.querySelector("#right_btn");
+// const images = document.querySelectorAll(".slide");
+
+// console.log(images.length - 1);
+
+// const startslider = 0;
+// const endslider = images.length - 1;
+
+// leftbtn.addEventListener("click", () => {
+//   alert("jellookvdnalskfg");
+// });
+
+// rightbtn.addEventListener("click", () => {
+//   startslider = startslider - 100;
+
+//   images.forEach((element) => {
+//     element.style.transform = `translateX(${startslider}%)`;
+//   });
+// });
